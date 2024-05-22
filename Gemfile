9cd701f9b988b014cf6db90596cc72bae804ee2f
Thiago Costa <dev_thiscosta@outlook.com>
2024-05-21 20:59:03 -0700

    parcial seed

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ gem "devise"
 gem 'devise-bootstrap-views', '~> 1.1'
 gem "faker"
 gem "pg_search"
+gem 'conecta_address_br'
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Seed Users
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address,
+    birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
+    reviews: Faker::Lorem.paragraph,
+    password: Faker::Internet.password
+  )
+end
+
+# Seed Cities
+cities = ConectaAddressBr::Cities.by_state_single("RJ")
+districts = ["Centro", "Zona Sul", "Zona Norte", "Zona Oeste"]
+cities.each do |city|
+  City.create!(
+    name: city,
+    district: districts.sample,
+    country: "Brasil",
+    state: "RJ",
+    safety_tip: Faker::Lorem.paragraph
+  )
+end
