44a4df8a0fd09633999744563d1dc2aeffb82751
Thiago Costa <dev_thiscosta@outlook.com>
2024-05-27 20:18:49 -0700

    some pages added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 User.destroy_all
 City.destroy_all
 DangerArea.destroy_all
+Review.destroy_all
 
 # Seed Users
 10.times do |i|
   User.create!(
     name: Faker::Name.name,
-    email: "teste#{i+1}@teste.com",
+    email: "teste#{i + 1}@teste.com",
     phone_number: Faker::PhoneNumber.phone_number,
     address: Faker::Address.full_address,
     birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
-    reviews: Faker::Lorem.paragraph,
     password: "123456",
     avatar: "https://kitt.lewagon.com/placeholder/users/ssaunier"
-      )
-    end
+  )
+end
 
 # Seed Cities
 cities = ConectaAddressBr::Cities.by_state_single("RJ")
@@ -31,16 +31,87 @@ end
 
 # Seed Danger Areas
 [
-  { name: "Complexo do Alemão", description: "Complexo de favelas localizado na Zona Norte do Rio de Janeiro", risk: rand(1..5), latitude: 123.456, longitude: 789.012 },
-  { name: "Cidade de Deus", description: "Favela localizada na Zona Oeste do Rio de Janeiro", risk: rand(1..5), latitude: 456.789, longitude: 12.345 },
-  { name: "Morro da Providência", description: "Favela localizada na região central do Rio de Janeiro", risk: rand(1..5), latitude: 789.012, longitude: 345.678 },
-  { name: "Rocinha", description: "Favela localizada na Zona Sul do Rio de Janeiro", risk: rand(1..5), latitude: 987.654, longitude: 321.098 },
-  { name: "Vidigal", description: "Favela localizada na Zona Sul do Rio de Janeiro", risk: rand(1..5), latitude: 654.321, longitude: 890.123 },
-  { name: "Maré", description: "Complexo de favelas localizado na Zona Norte do Rio de Janeiro", risk: rand(1..5), latitude: 234.567, longitude: 876.543 },
-  { name: "Jacarezinho", description: "Favela localizada na Zona Norte do Rio de Janeiro", risk: rand(1..5), latitude: 543.210, longitude: 901.234 },
-  { name: "Vila Kennedy", description: "Favela localizada na Zona Oeste do Rio de Janeiro", risk: rand(1..5), latitude: 876.543, longitude: 345.678 },
-  { name: "Ramos", description: "Bairro localizado na Zona Norte do Rio de Janeiro", risk: rand(1..5), latitude: 210.543, longitude: 543.210 },
-  { name: "Penha", description: "Bairro localizado na Zona Norte do Rio de Janeiro", risk: rand(1..5), latitude: 543.210, longitude: 876.543 }
+  {
+    name: "Complexo do Alemão",
+    description: "Complexo de favelas localizado na Zona Norte do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 123.456,
+    longitude: 789.012
+  },
+  {
+    name: "Cidade de Deus",
+    description: "Favela localizada na Zona Oeste do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 456.789,
+    longitude: 12.345
+  },
+  {
+    name: "Morro da Providência",
+    description: "Favela localizada na região central do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 789.012,
+    longitude: 345.678
+  },
+  {
+    name: "Rocinha",
+    description: "Favela localizada na Zona Sul do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 987.654,
+    longitude: 321.098
+  },
+  {
+    name: "Vidigal",
+    description: "Favela localizada na Zona Sul do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 654.321,
+    longitude: 890.123
+  },
+  {
+    name: "Maré",
+    description: "Complexo de favelas localizado na Zona Norte do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 234.567,
+    longitude: 876.543
+  },
+  {
+    name: "Jacarezinho",
+    description: "Favela localizada na Zona Norte do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 543.210,
+    longitude: 901.234
+  },
+  {
+    name: "Vila Kennedy",
+    description: "Favela localizada na Zona Oeste do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 876.543,
+    longitude: 345.678
+  },
+  {
+    name: "Ramos",
+    description: "Bairro localizado na Zona Norte do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 210.543,
+    longitude: 543.210
+  },
+  {
+    name: "Penha",
+    description: "Bairro localizado na Zona Norte do Rio de Janeiro",
+    risk: rand(1..5),
+    latitude: 543.210,
+    longitude: 876.543
+  }
 ].each do |danger_area|
   DangerArea.create!(danger_area)
 end
+
+# Seed Reviews
+User.all.each do |user|
+  50.times do
+    Review.create!(
+      comment: Faker::Lorem.paragraph,
+      user: user
+    )
+  end
+end
+puts "Seed completed!"
