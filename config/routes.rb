b26d6d0a65a5884669c9571a7330902d51b0a4c7
Leo Casas <digid@digid.com.br>
2024-06-04 22:04:09 -0300

    Improving navbar and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'cities/index'
-  get 'cities/show'
-  get 'index/show'
-  devise_for :users
+devise_for :users
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
